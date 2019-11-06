b94f0c20e239b3feb5e3fba156d6c332d729b71d
Nalyd1019 <dylan.osolian@gmail.com>
2019-11-06 16:03:44 +0100

    Ändring

--- workshop/src/HelloWorld.java
+++ workshop/src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args){new HelloWorld().program(); }
     void program() {
         System.out.println("Hello World");
+        System.out.println("Hello Again");
     }
 }
