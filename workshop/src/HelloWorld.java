1e6154a996843c2d3698f8e42516a392f8c699e3
Samdahl <Samdahl@protonmail.com>
2019-11-06 16:41:09 +0100

    ny

--- workshop/src/HelloWorld.java
+++ workshop/src/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Hello Again");
         System.out.println("merge");
+        System.out.println("intellij commit");
+        System.out.println("Fc barca > real madrid");
     }
 }
